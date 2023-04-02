260df10fdae91b91851822be04e4e9a9a55237f4
Z4RP1HR <103745705+Z4RP1HR@users.noreply.github.com>
2023-04-02 16:53:35 +0800

    Update troll.py

--- troll.py
+++ troll.py
@@ -9,6 +9,6 @@ os.system("xdg-open https://4anime.gg/")
 os.system("xdg-open https://check-host.net/")
 os.system("xdg-open https://www.tradingview.com/chart/mI1zfELI/?symbol=BYBIT%3ABTCUSDT.P")
 os.system("xdg-open https://www.youtube.com/")
-os.system("xdg-open https://anonprixor.pw")
+os.system("xdg-open https://www.chess.com/home")
 print("welcome to trolls hehehe")
 ##### Message Here #####
